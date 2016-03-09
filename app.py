01a3bc3a01a5b06c240d24cf6bc2ffce6498e957
readpeta <murakumo.room@gmail.com>
2016-03-09 19:21:29 +0900

    データベース周り

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 import MySQLdb
 from MySQLdb.cursors import DictCursor
 from collections import OrderedDict
 from flask import Flask, render_template, request, redirect, url_for
+import sys
+import codecs
 
 
 app = Flask(__name__)
 
+#sys.stdout = codecs.getwriter('utf_8')(sys.stdout) 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -14,8 +17,18 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     if(request.form['search-box']):
+        connection = MySQLdb.connect(db='camels', user='', passwd='')
+        cursor = connection.cursor()
         word = request.form['search-box']
-        return render_template('index.html', word=word)
+        cursor.execute('select * from indexs where title like ' + '"%' + word + '%"')
+        result = cursor.fetchall()
+        print result
+        if len(result) == 0:
+            result = "その検索ワードにはなにもマッチしませんでした。"
+        cursor.close()
+        connection.close()
+        return render_template('index.html', result=result)
+        #return render_template('index.html', title=result[0][1], text=result[0][2])
     else:
         word = ""
         return render_template('index.html')
